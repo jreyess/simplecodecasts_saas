349a9b46e8bd1a15c7906d1b2c088bb951abc929
Joshua Reyes <joshua.reyes@yrerson.ca>
2015-11-09 22:55:15 +0000

    hey

--- config/environment.rb
+++ config/environment.rb
@@ -3,18 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-# Initialize the Rails application.
-Rails.application.initialize!
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['joshua.reyes@ryerson.ca'],
-  :password => ENV['Jcr500178198'],
-  :domain => 'heroku.com',
-  :enable_startstls_auto => true
-}
